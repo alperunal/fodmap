b8a8067fee350e695317fd8cf3b7cf8748af98ac
Alper Unal <alper.91@hotmail.com>
2019-01-13 14:53:01 +0100

    Removed React-Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import FoodGuide from "./containers/FoodGuide/FoodGuide";
-import { Switch, Route } from "react-router-dom";
-import FoodList from "./components/FoodList/FoodList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route path="/:id" component={FoodList} />
-          <Route path="/" exact component={FoodGuide} />
-        </Switch>
+        <FoodGuide />
       </div>
     );
   }
